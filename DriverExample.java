661541c09e5d91d74a52ee317a79d1702e2c761e
Melanie Schwartz <101142745+melschwartz07@users.noreply.github.com>
2025-02-20 09:55:01 -0600

    commit

---
+++ DriverExample.java
@@ -0,0 +1,16 @@
+
+/* DriverIntroToJava
+ *
+ * Example class to practice compiling java programs
+ */
+public class DriverExample
+{
+  public static void main(String[] args)
+  {
+    int x = 10;
+
+    System.out.printf("\"%d\"\n", x);
+    System.out.printf("\"%7d\"\n", x);
+    System.out.printf("\"%f\"\n", x);  //JESS: Error on this line!
+  }
+}
